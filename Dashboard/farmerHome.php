e29887dddfb33f13fb402da0ace613ffd16ef43f
DishaKumari <dishakumari3298@gmail.com>
2019-03-16 20:33:25 +0000

    php profile name disp fix check

--- Dashboard/farmerHome.php
+++ Dashboard/farmerHome.php
@@ -171,7 +171,7 @@ $(document).ready(function(){
           <!-- Modal body -->
           <div class="modal-body">
             <img src="<?php $far['pic']; ?>" class="rounded mx-auto d-block" height="200px" width="200px" alt="Cinque Terre">
-            <h1>$far['name'];</h1>
+            <h1><?php $far['name']; ?></h1>
           </div>
           
           <!-- Modal footer -->
