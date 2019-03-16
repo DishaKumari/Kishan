90ffb3db8e7e4262cbcb65f75ea3f0c2ef2c1997
Rashmiranjan00 <rr.pradhan00@gmail.com>
2019-03-17 03:53:18 +0530

    ..

--- Dashboard/farmerHome.php
+++ Dashboard/farmerHome.php
@@ -554,20 +554,16 @@ $(document).ready(function(){
         </div>
     </div>
     <div id="map_canvas" style=" border: 2px solid #3872ac;"></div>
-            <script>
-              function initMap() {
-                  var cuttack = { lat: 20.4625, lng: 85.8830 };
-                  var cuttack1 = { lat: 20.4725, lng: 85.8530 };
-                  var map = new google.maps.Map(
-                      document.getElementById('map_canvas'), { zoom: 18, center: cuttack });
-                  var marker = new google.maps.Marker({ position: cuttack, map: map });
-                  var marker = new google.maps.Marker({ position: cuttack1, map: map });
-              }
-            </script>
-
-            <script async defer
-                src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBBvluQ1RpPOc_z2wsQd3xoJeDwUglLqxc&callback=initMap">
-              </script>
+    <script>
+      function initMap() {
+          var cuttack = { lat: 20.4625, lng: 85.8830 };
+          var cuttack1 = { lat: 20.4725, lng: 85.8530 };
+          var map = new google.maps.Map(
+              document.getElementById('map_canvas'), { zoom: 18, center: cuttack });
+          var marker = new google.maps.Marker({ position: cuttack, map: map });
+          var marker = new google.maps.Marker({ position: cuttack1, map: map });
+      }
+    </script>
     <!-- Body ended -->
     <br><br><br><br>
 
@@ -650,6 +646,8 @@ $(document).ready(function(){
 
   </footer>
   <!-- Footer -->
-  
+  <script async defer
+                src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBBvluQ1RpPOc_z2wsQd3xoJeDwUglLqxc&callback=initMap">
+  </script>
 </body>
 </html>
