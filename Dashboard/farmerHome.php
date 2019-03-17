7978e016f85f1126d0ab49a2369d9a573f2fe271
Rashmiranjan00 <rr.pradhan00@gmail.com>
2019-03-17 07:22:50 +0530

    Heading centered

--- Dashboard/farmerHome.php
+++ Dashboard/farmerHome.php
@@ -554,7 +554,7 @@ $(document).ready(function(){
         </div>
     </div>
     <h1>Land Location</h1>
-    <div class="container" id="map_canvas" style=" border: 2px solid #3872ac;"></div>
+    <div class="container txt-center" id="map_canvas" style=" border: 2px solid #3872ac;"></div>
     <script>
       function initMap() {
           var cuttack = { lat: 20.4625, lng: 85.8830 };
